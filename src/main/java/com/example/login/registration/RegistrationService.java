00f7b52490e79c156e6fc4fae971b611cbb9fa5b
sebchi-crtl <chiemelienwobodo@gmail.com>
2021-12-12 23:17:26 +0100

    fhh

---
+++ src/main/java/com/example/login/registration/RegistrationService.java
@@ -0,0 +1,34 @@
+package com.example.login.registration;
+
+import com.example.login.appuser.AppUser;
+import com.example.login.appuser.AppUserRole;
+import com.example.login.appuser.AppUserService;
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@AllArgsConstructor
+public class RegistrationService {
+
+    private final AppUserService appUserService;
+    private final EmailValidator emailValidator;
+
+    public String register(RegistrationRequest request) {
+        boolean isValidEmail = emailValidator.test(request.getEmail());
+
+        if(!isValidEmail){
+            throw new IllegalStateException("email not valid");
+        }
+
+        return appUserService.signUpUser(
+                new AppUser(
+                        request.getFirstName(),
+                        request.getLastName(),
+                        request.getEmail(),
+                        request.getPassword(),
+                        AppUserRole.USER
+
+                )
+        );
+    }
+}
